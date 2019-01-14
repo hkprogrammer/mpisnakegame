28260767179d616a157269da45d121ed16a6f008
hkprogrammer <hawaiihkprogrammer@gmail.com>
2019-01-13 19:22:35 -1000

    Gitignore fixed
    Signed-off-by: hkprogrammer <hawaiihkprogrammer@gmail.com>

--- index.js
+++ index.js
@@ -7,24 +7,11 @@ var data = fs.readFileSync(path);
 var score = JSON.parse(data);
 var player_score = score;
 
-
-// function loadGame(){
-//     console.log(JSON.stringify(score));
-// }
-
-
-
-
 function listening(request,response){
 
 	console.log("Listening");
 
 }
-
-
-
-
-
 
 app.use(express.static("website/LTS"));
 app.get('/add/:name/:score', addScore);
@@ -54,7 +41,7 @@ function addScore(request, response){
 			console.log("finished");
 
 		}
-		
+
 	}
 	else if(player_score[name]){
 		console.log("Name being taken");
@@ -65,13 +52,7 @@ function addScore(request, response){
 		}
 		//var new_data = JSON.stringify(player_score, null, 2);
 		//fs.writeFile(path, new_data, finished);
-		
-		
-
-
 	}
-	
+
 	response.send(reply);
-
-
 }
